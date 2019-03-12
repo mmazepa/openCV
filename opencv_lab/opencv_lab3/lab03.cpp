ed8b0a845e7f0916b9580e34dcff31672c282d23
mmazepa <maksymilianmariusz@gmail.com>
2019-03-12 23:25:13 +0100

    Wrzutka z zajęć (11.03.2019).

--- opencv_lab/opencv_lab3/lab03.cpp
+++ opencv_lab/opencv_lab3/lab03.cpp
@@ -7,19 +7,45 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat image;
+	Mat firstFrame, currentFrame, previousFrame;
+	Mat diff, eroded, dilated;
+	Mat gray, prev_gray;
+
 	VideoCapture cap;
-	cap.open(0);
+	cap.open("C:/Users/Mariusz/Desktop/opencv_tmp/robot_no_loop/robot_no_loop.avi");
 
 	namedWindow("window", CV_WINDOW_AUTOSIZE);
 
 	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
+	cap >> firstFrame;
+	currentFrame = firstFrame;
+	previousFrame = currentFrame;
+	diff = currentFrame;
+
 	while (1) {
 		try {
-			cap >> image;
-			imshow("window", image);
+			cap >> currentFrame;
+			if (currentFrame.empty()) {
+				cout << "The end, press ANY key to exit..." << endl;
+				_getch();
+				cap.release();
+				destroyAllWindows();
+				return 0;
+			}
+
+			cap >> diff;
+			cvtColor(currentFrame, gray, CV_RGB2GRAY);
+			cvtColor(previousFrame, prev_gray, CV_RGB2GRAY);
+			
+			absdiff(gray, prev_gray, diff);
+			threshold(gray, diff, 40, 255, THRESH_BINARY);
+			erode(diff, eroded, Mat(), Point(-1,-1), 5);
+			dilate(eroded, dilated, Mat(), Point(-1,-1), 3);
+
+			imshow("window", dilated);
+			previousFrame = currentFrame;
 		}
 		catch (Exception e) {
 			cap.open(1);
