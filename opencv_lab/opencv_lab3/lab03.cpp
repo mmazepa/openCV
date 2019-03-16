9fa5c19d4c8016ad1dbf0a67154aaa72735298c0
mmazepa <maksymilianmariusz@gmail.com>
2019-03-16 03:02:30 +0100

    Czujnik ruchu dla kamery i wideo + zarys do dodawania loga.

--- opencv_lab/opencv_lab3/lab03.cpp
+++ opencv_lab/opencv_lab3/lab03.cpp
@@ -21,23 +21,31 @@ String getPath(String videoName) {
 	return path + "/" + videoName + "/" + videoName + ".avi";
 }
 
-int main() {
+Mat motionDetection(VideoCapture cap, Mat currentFrame, Mat frameToCompare, int threshold_value, int erode_value, int dilate_value) {
+	Mat diff;
+	absdiff(frameToCompare, currentFrame, diff);
+	cvtColor(diff, currentFrame, CV_RGB2GRAY);
+	threshold(currentFrame, diff, threshold_value, 255, THRESH_BINARY);
+	erode(diff, diff, Mat(), Point(-1, -1), erode_value);
+	dilate(diff, diff, Mat(), Point(-1, -1), dilate_value);
+	return diff;
+}
+
+int motionDetector(VideoCapture cap, int choice) {
 	Mat firstFrame, currentFrame, previousFrame;
-	Mat diff, eroded, dilated;
-	Mat gray, prev_gray;
+	Mat diff;
 
-	VideoCapture cap;
-	cap.open(getPath("robot_no_loop"));
+	namedWindow("original", CV_WINDOW_AUTOSIZE);
+	namedWindow("modified", CV_WINDOW_AUTOSIZE);
 
-	namedWindow("window", CV_WINDOW_AUTOSIZE);
-
-	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	if (choice == 1 || choice == 2) {
+		cap.open(getPath("robot_no_loop"));
+	} else if (choice == 3 || choice == 4) {
+		cap.open(0);
+	}
 
 	cap >> firstFrame;
-	currentFrame = firstFrame;
-	previousFrame = currentFrame;
-	diff = currentFrame;
+	previousFrame = firstFrame.clone();
 
 	while (1) {
 		try {
@@ -48,17 +56,14 @@ int main() {
 				return 0;
 			}
 
-			cap >> diff;
-			cvtColor(currentFrame, gray, CV_RGB2GRAY);
-			cvtColor(previousFrame, prev_gray, CV_RGB2GRAY);
-			
-			absdiff(gray, prev_gray, diff);
-			threshold(gray, diff, 40, 255, THRESH_BINARY);
-			erode(diff, eroded, Mat(), Point(-1,-1), 5);
-			dilate(eroded, dilated, Mat(), Point(-1,-1), 3);
+			if (choice == 1) diff = motionDetection(cap, currentFrame, firstFrame, 20, 3, 1);
+			else if (choice == 2) diff = motionDetection(cap, currentFrame, previousFrame, 15, 1, 3);
+			else if (choice == 3) diff = motionDetection(cap, currentFrame, firstFrame, 20, 3, 1);
+			else if (choice == 4) diff = motionDetection(cap, currentFrame, previousFrame, 20, 3, 1);
 
-			imshow("window", dilated);
-			previousFrame = currentFrame;
+			imshow("original", currentFrame);
+			imshow("modified", diff);
+			previousFrame = currentFrame.clone();
 		}
 		catch (Exception e) {
 			cap.open(1);
@@ -67,6 +72,83 @@ int main() {
 			cleanUpOnExit(cap);
 			return 0;
 		}
+	}
+	return 0;
+}
+
+int addLogo(VideoCapture cap, int choice) {
+	Mat frame;
+
+	if (choice == 5) {
+		cap.open(getPath("robot_no_loop"));
+	}
+	else if (choice == 6) {
+		cap.open(0);
+	}
+
+	while (1) {
+		try {
+			cap >> frame;
+			if (frame.empty()) {
+				pressAnyKey();
+				cleanUpOnExit(cap);
+				return 0;
+			}
+		}
+		catch (Exception e) {
+			cap.open(1);
+		}
+		if (waitKey(15) == 27) {
+			cleanUpOnExit(cap);
+			return 0;
+		}
+	}
+	return 0;
+}
+
+int askForNumber(String question) {
+	int number;
+	cout << question << endl;
+	cin >> number;
+	return number;
+}
+
+void menu() {
+	cout << "MOTION DETECTION" << endl;
+	cout << "----------------" << endl;
+	cout << "MENU:" << endl;
+	cout << "1 - video (first frame)" << endl;
+	cout << "2 - video (previous frame)" << endl;
+	cout << "3 - camera (first frame)" << endl;
+	cout << "4 - camera (previous frame)" << endl;
+	cout << "5 - video (add logo)" << endl;
+	cout << "6 - camera (add logo)" << endl;
+	cout << "7 - exit" << endl;
+	cout << endl;
+}
+
+int main() {
+	VideoCapture cap;
+
+	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+	menu();
+	int choice = askForNumber("Your choice:");
+
+	if (choice < 5) {
+		motionDetector(cap, choice);
+	} else if (choice < 7) {
+		addLogo(cap, choice);
+	} else if (choice == 7) {
+		destroyAllWindows();
+		cout << "Good-bye!" << endl;
+		pressAnyKey();
+		return 0;
+	} else {
+		cout << "Unknown choice." << endl;
+		pressAnyKey();
+		return 0;
 	}
 	return 0;
 }
