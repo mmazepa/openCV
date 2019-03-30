ddbef8dc59688ca6b03d9bd7fd01c0240520582a
mmazepa <maksymilianmariusz@gmail.com>
2019-03-30 03:45:57 +0100

    Prace nad lab5.

--- opencv_lab/opencv_lab5/lab05.cpp
+++ opencv_lab/opencv_lab5/lab05.cpp
@@ -10,13 +10,13 @@ Mat frame, frame_gray;
 Mat dst, detected_edges;
 int lowThreshold = 50;
 const int max_lowThreshold = 100;
-int highThreshold = 150;
+int highThreshold = 90;
 const int max_highThreshold = 255;
 //const int ratio = 3;
 const int kernel_size = 3;
 
-const char* window_name = "window";
-const char* window_name2 = "Modified";
+const char* window_name = "Original";
+const char* window_name2 = "Colorized";
 const char* window_name3 = "Grad";
 const char* window_name4 = "Grad_X";
 const char* window_name5 = "Grad_Y";
@@ -30,10 +30,6 @@ void CannyThresholdChange() {
 	GaussianBlur(frame_gray, detected_edges, Size(3, 3), 0, 0, BORDER_DEFAULT);
 	Canny(detected_edges, detected_edges, lowThreshold, highThreshold, kernel_size);
 	imshow(window_name2, detected_edges);
-
-	//dst = Scalar::all(0);
-	//frame.copyTo(dst, detected_edges);
-	//imshow(window_name, dst);
 }
 
 void CannyThreshold(int, void*) {
@@ -44,12 +40,11 @@ int main() {
 	VideoCapture cap;
 	cap.open(0);
 
-	namedWindow("original", CV_WINDOW_AUTOSIZE);
-	//namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
 	namedWindow(window_name2, CV_WINDOW_AUTOSIZE);
-	//namedWindow(window_name3, CV_WINDOW_AUTOSIZE);
-	//namedWindow(window_name4, CV_WINDOW_AUTOSIZE);
-	//namedWindow(window_name5, CV_WINDOW_AUTOSIZE);
+	namedWindow(window_name3, CV_WINDOW_AUTOSIZE);
+	namedWindow(window_name4, CV_WINDOW_AUTOSIZE);
+	namedWindow(window_name5, CV_WINDOW_AUTOSIZE);
 
 	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
@@ -60,29 +55,52 @@ int main() {
 	Mat grad;
 	int scale = 1;
 	int delta = 0;
-	int ddepth = CV_16S;
+	int ddepth = CV_32F;
+
+	cap >> frame;
+	Mat image = Mat::zeros(frame.rows, frame.cols, CV_8UC3);
 
 	while (1) {
 		try {
 			cap >> frame;
 			dst.create(frame.size(), frame.type());
-			imshow("original", frame);		
+			imshow(window_name, frame);		
 			CannyThresholdChange();
 
+			GaussianBlur(frame_gray, frame_gray, Size(7, 7), 0, 0, BORDER_DEFAULT);
+
 			Sobel(frame_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
-			convertScaleAbs(grad_x, abs_grad_x);
-			
 			Sobel(frame_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+
+			convertScaleAbs(grad_x, abs_grad_x);
 			convertScaleAbs(grad_y, abs_grad_y);
 
-			//addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+			Mat orientation = Mat::zeros(image.rows, image.cols, CV_32F);
+			phase(grad_x, grad_y, orientation, true);
 
 			Mat res_gx, res_gy;
 			threshold(abs_grad_x, res_gx, lowThreshold, 255, THRESH_BINARY);
 			threshold(abs_grad_y, res_gy, lowThreshold, 255, THRESH_BINARY);
-			addWeighted(res_gx, 0.5, res_gy, 0.5, 0, grad);
 
-			imshow(window_name3, grad);
+			Vec3b col1 = Vec3b(255, 255, 255);
+			Vec3b col2 = Vec3b(255, 0, 0);
+			Vec3b col3 = Vec3b(0, 255, 0);
+			Vec3b col4 = Vec3b(0, 0, 255);
+			Vec3b col5 = Vec3b(0, 0, 0);
+
+			for (int y = 0; y < orientation.rows; y++) {
+				for (int x = 0; x < orientation.cols; x++) {	
+					double orient = (double)orientation.at<float>(Point(x, y));
+
+					if (orient > 45 && orient <= 135) image.at<Vec3b>(Point(x, y)) = col1;
+					else if (orient > 135 && orient <= 225) image.at<Vec3b>(Point(x, y)) = col2;
+					else if (orient > 225 && orient <= 315) image.at<Vec3b>(Point(x, y)) = col3;
+					else if (orient > 315 && orient <= 360) image.at<Vec3b>(Point(x, y)) = col4;
+					else image.at<Vec3b>(Point(x, y)) = col5;
+				}
+			}
+
+			imshow(window_name3, image);
 			imshow(window_name4, res_gx);
 			imshow(window_name5, res_gy);
 		}
