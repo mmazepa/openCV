68e157462cb8b7e18f43a70c298d7c07a3e790a9
mmazepa <maksymilianmariusz@gmail.com>
2019-05-10 02:02:02 +0200

    Cenzura (poprawiony prostokąt + rozmycie) i skróty klawiszowe.

--- opencv_lab/opencv_lab10/lab10.cpp
+++ opencv_lab/opencv_lab10/lab10.cpp
@@ -3,12 +3,15 @@
 #include<opencv2/objdetect/objdetect.hpp>
 #include<iostream>
 #include<conio.h>
+#include<windows.h>
 
 using namespace cv;
 using namespace std;
 
 Mat frame, gray;
 VideoCapture cap;
+
+int choice = 1;
 
 String path = "M:/Programy/OpenCV/opencv/sources/data/haarcascades/";
 CascadeClassifier face_cascade = CascadeClassifier(path + "haarcascade_frontalface_default.xml");
@@ -26,6 +29,50 @@ int minNeighbors_max = 100;
 int minSize = 5;
 int minSize_max = 100;
 
+void textWithShadow(string text, Point point, Scalar shadowColor) {
+	Point shadow1 = Point(point.x + 1, point.y + 1);
+	Point shadow2 = Point(point.x + 1, point.y - 1);
+	Point shadow3 = Point(point.x - 1, point.y + 1);
+	Point shadow4 = Point(point.x - 1, point.y - 1);
+	putText(frame, text, shadow1, FONT_HERSHEY_PLAIN, 1.0, shadowColor);
+	putText(frame, text, shadow2, FONT_HERSHEY_PLAIN, 1.0, shadowColor);
+	putText(frame, text, shadow3, FONT_HERSHEY_PLAIN, 1.0, shadowColor);
+	putText(frame, text, shadow4, FONT_HERSHEY_PLAIN, 1.0, shadowColor);
+	putText(frame, text, point, FONT_HERSHEY_PLAIN, 1.0, Scalar(255, 255, 255));
+}
+
+void putTrackbarRealValuesOnFrame() {
+	string mode;
+	switch (choice) {
+		case 1:
+			mode = "Face Detection";
+			break;
+		case 2:
+			mode = "Gaussian Blur";
+			break;
+		case 3:
+			mode = "Black Rectangle";
+			break;
+		case 4:
+			mode = "Face Copying";
+			break;
+	}
+	textWithShadow("Choice:", Point(5, 15), Scalar(155, 0, 0));
+	textWithShadow(mode, Point(75, 15), Scalar(0, 100, 0));
+}
+
+int choiceChange() {
+	if (GetAsyncKeyState('1') & 0x8000)
+		choice = 1;
+	else if (GetAsyncKeyState('2') & 0x8000)
+		choice = 2;
+	else if (GetAsyncKeyState('3') & 0x8000)
+		choice = 3;
+	else if (GetAsyncKeyState('4') & 0x8000)
+		choice = 4;
+	return choice;
+}
+
 void ValueChanger() {
 	vector<Rect> faces;
 	face_cascade.detectMultiScale(gray, faces);
@@ -34,25 +81,37 @@ void ValueChanger() {
 	for (int i = 0; i < faces.size(); i++) {
 		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
 		int face_radius = cvRound((faces[i].width + faces[i].height)*0.25);
-		circle(frame, center, face_radius, Scalar(0, 100, 0), 3);
-		//GaussianBlur(frame, frame, Size(faces[i].width / 2, faces[i].height / 2), 3);
+
+		if (choice == 1)
+			circle(frame, center, face_radius, Scalar(0, 100, 0), 3);
+
+		if (choice == 2)
+			GaussianBlur(frame(faces[i]), frame(faces[i]), Size(0, 0), 5);
 
 		Mat faceROI = gray(faces[i]);
 		vector<Rect> eyes;
 		eyes_cascade.detectMultiScale(faceROI, eyes);
 		//eyes_cascade.detectMultiScale(faceROI, eyes, (double)scaleFactor, minNeighbors, 0, Size(minSize, minSize));
-
-		for (int j = 0; j < eyes.size(); j++) {
-			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			int eye_radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-			circle(frame, eye_center, eye_radius, Scalar(0, 0, 200), 3);
+	
+		if (choice == 1) {
+			for (int j = 0; j < eyes.size(); j++) {
+				Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+				int eye_radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+				circle(frame, eye_center, eye_radius, Scalar(0, 0, 200), 3);
+			}
 		}
 
-		if (eyes.size() == 2) {
-			int width = faces[i].x + faces[i].width/2;
-			int height = faces[i].y + eyes[0].height;
-			Rect censored_black = Rect(faces[i].x + eyes[0].x - 75, faces[0].y + eyes[0].y, width*0.75, height*0.5);
-			rectangle(frame, censored_black, Scalar(0, 0, 0), CV_FILLED);
+		if (choice == 3) {
+			if (eyes.size() == 2) {
+				Point eye_center1(faces[i].x + eyes[0].x + eyes[0].width / 2, faces[i].y + eyes[0].y + eyes[0].height / 2);
+				Point eye_center2(faces[i].x + eyes[1].x + eyes[1].width / 2, faces[i].y + eyes[1].y + eyes[1].height / 2);
+
+				Point bottomLeft(min(eye_center1.x, eye_center2.x)*0.8, min(eye_center1.y, eye_center2.y)*0.9);
+				Point topRight(max(eye_center1.x, eye_center2.x)*1.2, max(eye_center1.y, eye_center2.y)*1.1);
+
+				Rect censored_black = Rect(bottomLeft, topRight);
+				rectangle(frame, censored_black, Scalar(0, 0, 0), CV_FILLED);
+			}
 		}
 	}
 }
@@ -66,8 +125,8 @@ int main() {
 
 	namedWindow("window", CV_WINDOW_AUTOSIZE);
 
-	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	double dWidth = cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	double dHeight = cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
 
 	//createTrackbar("scaleFactor", "window", &scaleFactor, scaleFactor_max, ValueChange);
 	//createTrackbar("minNeighbors", "window", &minNeighbors, minNeighbors_max, ValueChange);
@@ -79,6 +138,8 @@ int main() {
 			cvtColor(frame, gray, CV_RGB2GRAY);
 			equalizeHist(gray, gray);
 
+			choiceChange();
+			putTrackbarRealValuesOnFrame();
 			ValueChanger();
 
 			imshow("window", frame);
